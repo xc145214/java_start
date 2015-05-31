759dc1333b207f6436eed198cdd7e33efa827034
xc145214 <xc145214@gmail.com>
2015-05-31 23:30:17 +0800

    异常处理

---
+++ javaApp/src/day11/InnerClassTest.java
@@ -0,0 +1,30 @@
+package day11;
+
+interface Inter {
+    void show();
+}
+
+class Outer {
+    //补足代码 (建议用匿名内部类)
+
+    static Inter method() {
+        return new Inter() {
+            public void show() {
+            }
+        };
+    }
+}
+
+/*
+class haha implements Inter
+{
+	public void show(){}
+}
+*/
+class InnerClassTest {
+    public static void main(String[] args) {
+        Outer.method().show();
+
+
+    }
+}
