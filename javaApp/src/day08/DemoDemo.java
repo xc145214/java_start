0ec05c8557615a1dd92491ea065ced14107dbb6f
xc145214 <xc145214@gmail.com>
2015-05-31 22:57:45 +0800

    面向对象

---
+++ javaApp/src/day08/DemoDemo.java
@@ -0,0 +1,15 @@
+package day08;
+
+class Demo {
+    static Demo d = new Demo();
+
+    Demo() {
+
+    }
+}
+
+class DemoDemo {
+    public static void main(String[] args) {
+        new Demo();//StackOverflowError
+    }
+}
