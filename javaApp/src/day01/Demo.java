11ac374a7157acc5a32291af59775b5333fe8613
xc145214 <xc145214@gmail.com>
2015-05-31 21:09:53 +0800

    hello world

---
+++ javaApp/src/day01/Demo.java
@@ -0,0 +1,11 @@
+package day01;
+
+/**
+ * Created by 川 on 2015/5/31.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
